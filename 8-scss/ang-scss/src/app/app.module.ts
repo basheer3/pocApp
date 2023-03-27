8491269e719d564773887e30ffaf3cbb3d9bcfe6
basheer <basheerkhanxyz@gmail.com>
2023-03-27 18:34:47 +0530

    updates

---
+++ 8-scss/ang-scss/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PaymentComponent } from './payment/payment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
